dcb6bd01753b4a3c11fd0b0764eceeffca6364d5
Tor Morten Jensen <tormorten@tormorten.no>
2021-02-10 12:35:24 +0100

    wip

--- src/Support/Packages.php
+++ src/Support/Packages.php
@@ -12,7 +12,6 @@ class Packages
     protected function prepare()
     {
         if (!$this->packages) {
-            var_dump('hei');
             $this->packages = new Collection(json_decode(
                 file_get_contents(
                     rtrim(Config::get('mix.home'), '/') . '/vendor/composer/installed.json'
